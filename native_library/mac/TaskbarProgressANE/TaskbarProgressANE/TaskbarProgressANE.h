60b2d508b36949a25115126c512cf174844f4440
Eoin Landy <el111@yahoo.com>
2017-06-10 18:40:28 +0100

    0.0.2

---
+++ native_library/mac/TaskbarProgressANE/TaskbarProgressANE/TaskbarProgressANE.h
@@ -0,0 +1,19 @@
+//
+//  TaskbarProgressANE.h
+//  TaskbarProgressANE
+//
+//  Created by Eoin Landy on 10/06/2017.
+//  Copyright © 2017 Tua Rua Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TaskbarProgressANE.
+FOUNDATION_EXPORT double TaskbarProgressANEVersionNumber;
+
+//! Project version string for TaskbarProgressANE.
+FOUNDATION_EXPORT const unsigned char TaskbarProgressANEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskbarProgressANE/PublicHeader.h>
+
+
