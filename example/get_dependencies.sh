4d916d3a15bf44d0716563bfae68d38f61823e4c
Eoin Landy <info@tuarua.co.uk>
2020-09-26 16:01:47 +0100

    use air-tools

--- example/get_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-FreSharpVersion="2.4.0"
-FreSwiftVersion="4.5.0"
-
-wget -O ../native_extension/ane/FreSwift.ane https://github.com/tuarua/Swift-IOS-ANE/releases/download/${FreSwiftVersion}/FreSwift.ane?raw=true
-wget -O ../native_extension/ane/FreSharp.ane https://github.com/tuarua/FreSharp/releases/download/${FreSharpVersion}/FreSharp.ane?raw=true
